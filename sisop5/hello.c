6d3a46ca8b01eae035bc8112ecfd66539b418bf8
k2biru <fahrizal.hari@gmail.com>
2018-11-27 15:51:59 +0700

    Bab 5

---
+++ sisop5/hello.c
@@ -0,0 +1,13 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+int init_module(void)
+{
+ printk(KERN_INFO "Hello world 1.\n");
+ return 0;
+}
+void cleanup_module(void)
+{
+ printk(KERN_INFO "Goodbye world 1.\n");
+}
+
